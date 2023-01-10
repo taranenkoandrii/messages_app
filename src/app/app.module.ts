94e29b076f486f31b6396228045791b5fb2d407f
Taranenko Andrii <taranenkoandrii08@gmail.com>
2023-01-10 20:31:52 +0200

    add firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { AngularFireModule } from '@angular/fire/compat';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,10 @@ import { StoreModule } from '@ngrx/store';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        projectId: 'messages-app-561f1',
+        appId: '1:159749462969:web:51d080e8877b248d53b468',
+        storageBucket: 'messages-app-561f1.appspot.com',
+        locationId: 'europe-west',
+        apiKey: 'AIzaSyDz5EzHWA6pk946KwBiSwupeA0YPOjAN6s',
+        authDomain: 'messages-app-561f1.firebaseapp.com',
+        messagingSenderId: '159749462969',
+    }
+};
