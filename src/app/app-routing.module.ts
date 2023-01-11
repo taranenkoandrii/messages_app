90750f89023f8c2b2801ac9fd41871cd47aa1140
Andrii Taranenko <taranenkoandrii08@gmail.com>
2023-01-11 20:25:01 +0200

    change init page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '', redirectTo: 'messages', pathMatch: 'full'},
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'messages', loadChildren: () => import('./messages/messages.module').then(m => m.MessagesModule) }
 ];
